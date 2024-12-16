13a97a7ef8109232138467449caeeef40d3b70bc
Santiago Rivero <santiago.rivero@outlook.com>
2024-12-16 12:15:14 -0400

    - added apointment reservation system -finished med selection

--- Sistema Medico/Form4.Designer.cs
+++ Sistema Medico/Form4.Designer.cs
@@ -32,6 +32,24 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.general = new System.Windows.Forms.Label();
+            this.neurologia = new System.Windows.Forms.Label();
+            this.ginecologia = new System.Windows.Forms.Label();
+            this.urologia = new System.Windows.Forms.Label();
+            this.dermatologia = new System.Windows.Forms.Label();
+            this.traumatologia = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label2
@@ -71,11 +89,209 @@
             this.label1.TabIndex = 7;
             this.label1.Text = "Datos del paciente:";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 44);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Medicina general";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 44);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Neurologia";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 386);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 44);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Ginecologia";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(388, 386);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 44);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Traumatologia";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(388, 214);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(128, 44);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Urologia";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(388, 304);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(128, 44);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Dermatologia";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 18);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "50 Bs.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 351);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 18);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "70 Bs.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 433);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 18);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "80 Bs.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(385, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 18);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "80 Bs.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(385, 351);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 18);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "100 Bs.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(385, 433);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 18);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "120 Bs.";
+            // 
+            // general
+            // 
+            this.general.AutoSize = true;
+            this.general.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.general.Location = new System.Drawing.Point(146, 226);
+            this.general.Name = "general";
+            this.general.Size = new System.Drawing.Size(127, 18);
+            this.general.TabIndex = 20;
+            this.general.Text = "Pacientes: 0/10";
+            // 
+            // neurologia
+            // 
+            this.neurologia.AutoSize = true;
+            this.neurologia.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.neurologia.Location = new System.Drawing.Point(146, 316);
+            this.neurologia.Name = "neurologia";
+            this.neurologia.Size = new System.Drawing.Size(18, 18);
+            this.neurologia.TabIndex = 21;
+            this.neurologia.Text = "h";
+            // 
+            // ginecologia
+            // 
+            this.ginecologia.AutoSize = true;
+            this.ginecologia.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ginecologia.Location = new System.Drawing.Point(146, 398);
+            this.ginecologia.Name = "ginecologia";
+            this.ginecologia.Size = new System.Drawing.Size(18, 18);
+            this.ginecologia.TabIndex = 22;
+            this.ginecologia.Text = "h";
+            // 
+            // urologia
+            // 
+            this.urologia.AutoSize = true;
+            this.urologia.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.urologia.Location = new System.Drawing.Point(522, 226);
+            this.urologia.Name = "urologia";
+            this.urologia.Size = new System.Drawing.Size(18, 18);
+            this.urologia.TabIndex = 23;
+            this.urologia.Text = "h";
+            // 
+            // dermatologia
+            // 
+            this.dermatologia.AutoSize = true;
+            this.dermatologia.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dermatologia.Location = new System.Drawing.Point(522, 316);
+            this.dermatologia.Name = "dermatologia";
+            this.dermatologia.Size = new System.Drawing.Size(18, 18);
+            this.dermatologia.TabIndex = 24;
+            this.dermatologia.Text = "h";
+            // 
+            // traumatologia
+            // 
+            this.traumatologia.AutoSize = true;
+            this.traumatologia.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.traumatologia.Location = new System.Drawing.Point(522, 398);
+            this.traumatologia.Name = "traumatologia";
+            this.traumatologia.Size = new System.Drawing.Size(18, 18);
+            this.traumatologia.TabIndex = 25;
+            this.traumatologia.Text = "h";
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(756, 516);
+            this.Controls.Add(this.traumatologia);
+            this.Controls.Add(this.dermatologia);
+            this.Controls.Add(this.urologia);
+            this.Controls.Add(this.ginecologia);
+            this.Controls.Add(this.neurologia);
+            this.Controls.Add(this.general);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.dateTimePicker1);
@@ -94,5 +310,23 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label general;
+        private System.Windows.Forms.Label neurologia;
+        private System.Windows.Forms.Label ginecologia;
+        private System.Windows.Forms.Label urologia;
+        private System.Windows.Forms.Label dermatologia;
+        private System.Windows.Forms.Label traumatologia;
     }
 }
--- Sistema Medico/Form4.cs
+++ Sistema Medico/Form4.cs
@@ -17,10 +17,14 @@ namespace Sistema_Medico
     {
         private readonly MaterialSkinManager materialSkinManager;
         private string correoPaciente;
+        private string archivoCitas = "citas.txt";
+        private Dictionary<string, int> contadorEspecialidades = new Dictionary<string, int>();
+        private const int limiteCitas = 10;
         public Form4(string correoPaciente)
         {
             InitializeComponent();
             this.Text = "Sistema medico";
+            this.correoPaciente=correoPaciente;
             materialSkinManager = MaterialSkinManager.Instance;
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
@@ -31,7 +35,9 @@ namespace Sistema_Medico
                 Accent.Orange700,
                 TextShade.WHITE
             );
-            this.correoPaciente=correoPaciente;
+            InicializarContadorEspecialidades();
+            CargarCitas();
+            ActualizarLabels();
         }
 
         private void Form4_Load(object sender, EventArgs e)
@@ -59,7 +65,105 @@ namespace Sistema_Medico
 
             MessageBox.Show("No se encontraron datos para el correo ingresado.");
         }
+        private void InicializarContadorEspecialidades()
+        {
+            contadorEspecialidades["Medicina General"] = 0;
+            contadorEspecialidades["Urología"] = 0;
+            contadorEspecialidades["Neurología"] = 0;
+            contadorEspecialidades["Dermatología"] = 0;
+            contadorEspecialidades["Ginecología"] = 0;
+            contadorEspecialidades["Traumatología"] = 0;
+        }
+        private void CargarCitas()
+        {
+            if (File.Exists(archivoCitas))
+            {
+                foreach (string linea in File.ReadLines(archivoCitas))
+                {
+                    var datos = linea.Split(';');
+                    if (datos.Length == 2)
+                    {
+                        string especialidad = datos[0];
+                        int cantidad = int.Parse(datos[1]);
+                        if (contadorEspecialidades.ContainsKey(especialidad))
+                        {
+                            contadorEspecialidades[especialidad] = cantidad;
+                        }
+                    }
+                }
+            }
+        }
+        private void GuardarCitas()
+        {
+            using (StreamWriter sw = new StreamWriter(archivoCitas))
+            {
+                foreach (var especialidad in contadorEspecialidades)
+                {
+                    sw.WriteLine($"{especialidad.Key};{especialidad.Value}");
+                }
+            }
+        }
+        private void ActualizarLabels()
+        {
+            general.Text = $"{contadorEspecialidades["Medicina General"]}/10";
+            urologia.Text = $"{contadorEspecialidades["Urología"]}/10";
+            neurologia.Text = $"{contadorEspecialidades["Neurología"]}/10";
+            dermatologia.Text = $"{contadorEspecialidades["Dermatología"]}/10";
+            ginecologia.Text = $"{contadorEspecialidades["Ginecología"]}/10";
+            traumatologia.Text = $"{contadorEspecialidades["Traumatología"]}/10";
+        }
+        private void ConfirmarCita(string especialidad)
+        {
+            if (contadorEspecialidades.Values.Sum() >= limiteCitas)
+            {
+                MessageBox.Show("Se ha alcanzado el límite de 10 pacientes por día.", "Límite alcanzado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
 
+            if (contadorEspecialidades.ContainsKey(especialidad))
+            {
+                if (contadorEspecialidades[especialidad] < 10)
+                {
+                    contadorEspecialidades[especialidad]++;
+                    GuardarCitas();
+                    ActualizarLabels();
+                    MessageBox.Show($"Cita confirmada en {especialidad}.\nTotal pacientes: {contadorEspecialidades[especialidad]}", "Cita Confirmada");
+                }
+                else
+                {
+                    MessageBox.Show($"La especialidad {especialidad} ya tiene 10 pacientes.", "Límite alcanzado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConfirmarCita("Medicina General");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ConfirmarCita("Neurología");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ConfirmarCita("Ginecología");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ConfirmarCita("Urología");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ConfirmarCita("Dermatología");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ConfirmarCita("Traumatología");
+        }
     }
 }
